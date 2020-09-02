39e11721312422fe456270c843f3629804bc9697
Mars Dong <kdong007@hotmail.com>
2020-09-01 21:10:17 -0700

    build

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const react_1 = tslib_1.__importDefault(require("react"));
+const react_native_1 = require("react-native");
+const TestComponent = () => <react_native_1.Text>Test test</react_native_1.Text>;
+exports.default = TestComponent;
+//# sourceMappingURL=index.js.map
